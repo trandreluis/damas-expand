dd5bc614b79f9f50d9607805d08b49962f93f764
Andre Luis <tr.andreluis@gmail.com>
2017-05-08 05:57:43 -0300

    Adicao da checagem de de posicionamento das pecas

--- fachada-de-testes/IFacadeJogoDamas.java
+++ fachada-de-testes/IFacadeJogoDamas.java
@@ -7,8 +7,17 @@ public interface IFacadeJogoDamas {
 	 */
 	public void zerarJogo();
 
-	public void iniciarPartida(String idPartida, int dimensaoTabuleiro, String jogadorUm, String jogadorDois, boolean comerVoltando,
-			boolean sopro) throws DamasException;
+	/**
+	 * Remove todas as peças do tabuleiro da partida, deixando-o vazio
+	 * 
+	 * @param idTabuleiro
+	 *            ID da partida a ter o tabuleiro limpo.
+	 * 
+	 */
+	public void limparTabuleiro(String idPartida);
+
+	public void iniciarPartida(String idPartida, int dimensaoTabuleiro, String jogadorUm, String jogadorDois,
+			boolean comerVoltando, boolean sopro) throws DamasException;
 
 	/**
 	 * Insere uma peça no tabuleiro a partir da posição passada
@@ -17,7 +26,7 @@ public interface IFacadeJogoDamas {
 	 * (representadas pelos numeros inteiros 1 e 2) que representam
 	 * respectivamente as cores branca e preta.
 	 */
-	public void posicionarPeca(int cor, String linha, int coluna) throws DamasException;
+	public void posicionarPeca(String idPartida, int cor, String linha, int coluna) throws DamasException;
 
 	/**
 	 * Criar tabuleiro
@@ -25,27 +34,27 @@ public interface IFacadeJogoDamas {
 	 * @param dimensao
 	 *            Dimensao que o tabuleiro criado terá¡ (Ex: para uma dimensao =
 	 *            6 o tabuleiro terá¡ 6 casas de largura x 6 casas de altura).
-	 * @return 
+	 * @return
 	 *
 	 * @throws DamasException
 	 *             Exceção que pode ser lançada em caso de descomprimento das
 	 *             regras pré-estabelicidas para a aplicação
 	 *
 	 */
-	
+
 	public String getCorCasa(String idTabuleiro, int linha, String coluna) throws DamasException;
-	
+
 	/**
-	 * Realiza o movimento - jogada - de um respectivo jogador com os
-	 * parâmetros passados (Ex: numeroJogador = 1, linhaOrigem = B,
-	 * colunaOrigem = 1, linhaDestino = C, colunaDestino = 2)
+	 * Realiza o movimento - jogada - de um respectivo jogador com os parâmetros
+	 * passados (Ex: numeroJogador = 1, linhaOrigem = B, colunaOrigem = 1,
+	 * linhaDestino = C, colunaDestino = 2)
 	 *
 	 * @param numeroJogador
 	 *            Numero que indentifica o jogador que vai realizar o movimento
 	 *            (Ex: numeroJogador = 2)
 	 * @param linhaOrigem
-	 *            Linha de origem da peça que será movimentada (Ex:
-	 *            linhaOrigem = C)
+	 *            Linha de origem da peça que será movimentada (Ex: linhaOrigem
+	 *            = C)
 	 * @param colunaOrigem
 	 *            Coluna de origem da peça que será movimentada (Ex:
 	 *            colunaOrigem = 4)
@@ -72,13 +81,14 @@ public interface IFacadeJogoDamas {
 	 *            Letra que representa a linha do tabuleiro (Ex: B)
 	 * @param coluna
 	 *            Número que representa a coluna do tabuleiro
+	 * @return 
 	 *
 	 * @throws DamasException
 	 *             Exceção que pode ser lançada em caso de descomprimento das
 	 *             regras pré-estabelicidas para a aplicação
 	 *
 	 */
-	public void isOcupada(int linha, String coluna) throws DamasException;
+	public boolean isOcupada(String idPartida, int linha, String coluna) throws DamasException;
 
 	/**
 	 * Recupera e retorna a dimensão do tabuleiro
